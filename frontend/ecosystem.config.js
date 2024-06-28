f6924d50b595dc77e95e7a3311954e7249417328
edgar-ianke <edgaryanke@yandex.ru>
2024-06-28 09:41:25 +0300

    Деплой фронта

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend npm i && npm run build',
+      'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
 };
