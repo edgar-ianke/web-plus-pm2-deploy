c600f8bbc837871d91e49a06d195c41434e43d06
edgar-ianke <edgaryanke@yandex.ru>
2024-06-28 08:35:09 +0300

    1

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'cd frontend npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     },
   },
 };
